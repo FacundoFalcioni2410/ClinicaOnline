4cc8d9e11a6b277df015d08684481ceda19ccc8c
FacundoFalcioni2410 <facundofalcioni2410@gmail.com>
2021-10-21 11:12:00 -0300

    implementacion de captcha

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -26,6 +26,9 @@ import { SharedModule } from './modules/shared/shared.module';
 import { SinPermisosComponent } from './components/sin-permisos/sin-permisos.component';
 import { MiPerfilComponent } from './components/mi-perfil/mi-perfil.component';
 import { MisHorariosComponent } from './components/mis-horarios/mis-horarios.component';
+import { RecaptchaFormsModule, RecaptchaModule } from 'ng-recaptcha';
+import { RECAPTCHA_V3_SITE_KEY, RecaptchaV3Module } from "ng-recaptcha";
+
 
 @NgModule({
   declarations: [
@@ -51,14 +54,19 @@ import { MisHorariosComponent } from './components/mis-horarios/mis-horarios.com
       preventDuplicates: true,
     }),
     LazyLoadImageModule,
-    SharedModule
+    SharedModule,
+    RecaptchaModule,
+    RecaptchaV3Module,
     // SweetAlert2Module.forRoot()
   ],
   providers: [
     {
       provide: LAZYLOAD_IMAGE_HOOKS,
       useClass: ScrollHooks
-    }
+    },
+    {
+      provide: RECAPTCHA_V3_SITE_KEY,
+      useValue: "6LfHsOUcAAAAAOmlfLmi2XN9CN1khhahwkEwAUkr" }
   ],
   bootstrap: [AppComponent]
 })
--- src/app/components/registro/registro.component.ts
+++ src/app/components/registro/registro.component.ts
@@ -8,6 +8,7 @@ import { Router } from '@angular/router';
 import { FirestoreService } from 'src/app/services/firestore.service';
 import Swal from 'sweetalert2'
 import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { ReCaptchaV3Service } from 'ng-recaptcha';
 
 @Component({
   selector: 'app-registro',
@@ -42,7 +43,7 @@ export class RegistroComponent implements OnInit {
     }
   }
 
-  constructor(public auth: AuthService, private formBuilder: FormBuilder, private firebaseStorage: AngularFireStorage, private router: Router, private toaster: ToastrService, private firestore: FirestoreService) {
+  constructor(public auth: AuthService, private formBuilder: FormBuilder, private firebaseStorage: AngularFireStorage, private router: Router, private toaster: ToastrService, private firestore: FirestoreService, private recaptchaV3Service: ReCaptchaV3Service) {
     this.form = this.formBuilder.group({});
     this.formData = new FormData();
     this.getEspecialidades();
@@ -250,5 +251,10 @@ export class RegistroComponent implements OnInit {
         this.auth.loading = false;
       },1500);
     });
+  }
+
+  executeImportantAction(): void {
+    this.recaptchaV3Service.execute('importantAction')
+      .subscribe((token) => console.log(token));
   }
 }
